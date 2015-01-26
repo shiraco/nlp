927ffc8632cd9737df65fa56cebc26bb07d96689
Koji Shiraishi <shiraco@gmail.com>
2015-01-26 12:26:58 +0900

    Create word_count

---
+++ nlp/word_count.py
@@ -0,0 +1,51 @@
+import sys
+from collections import defaultdict
+
+
+class WordCount:
+
+    def __init__(self):
+        self.counts = defaultdict(lambda: 0)
+
+    def count(self, raw_text_file_name):
+        with open(raw_text_file_name, "r") as f:
+            for line in f:
+                line = line.strip()
+                if len(line) != 0:
+                    words = line.split()
+                    for w in words:
+                        self.counts[w] += 1
+        return self
+
+    def to_file(self, word_counted_file_name):
+        with open(word_counted_file_name, "w") as f:
+            for word, count in sorted(self.counts.items()):
+                f.write("{0}\t{1}\n".format(word, count))
+
+    @classmethod
+    def read_file(cls, word_counted_file_name):
+        counts = defaultdict(lambda: 0)
+        with open(word_counted_file_name, "r") as f:
+            for line in f:
+                line = line.strip()
+                if len(line) != 0:
+                    w = line.split()
+                    if len(w) == 2:
+                        word, count = w[0], int(w[1])
+                        counts[word] = count
+                    else:
+                        counts = defaultdict(lambda: 0)
+                        break
+
+        return counts
+
+
+if __name__ == "__main__":
+    in_file_name = sys.argv[1]
+    out_file_name = sys.argv[2]
+
+    if len(sys.argv) != 2:
+        quit()
+
+    wc = WordCount()
+    wc.count(in_file_name).to_file(out_file_name)
---
+++ test/test_word_count.py
@@ -0,0 +1,15 @@
+import unittest
+from nlp.word_count import WordCount
+
+
+class TestWordCount(unittest.TestCase):
+
+    def test_count(self):
+        wc = WordCount()
+        actual = wc.count("test/resource/00-input.txt").counts
+        expected = WordCount.read_file("test/resource/00-answer.txt")
+        self.assertEqual(dict(actual), dict(expected))
+
+
+if __name__ == "__main__":
+    unittest.main()
